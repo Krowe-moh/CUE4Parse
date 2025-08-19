e5d7d1b0853b4d735cb0f5bf94cee1f4a03c70f5
Krowe Moh <27891447+Krowe-moh@users.noreply.github.com>
2025-08-19 21:10:46 +1000

    Update FCompressedChunk.cs

--- CUE4Parse/UE4/Assets/Objects/FCompressedChunk.cs
+++ CUE4Parse/UE4/Assets/Objects/FCompressedChunk.cs
@@ -5,8 +5,8 @@ namespace CUE4Parse.UE4.Assets.Objects
 {
     public readonly struct FCompressedChunk
     {
-        public readonly int UncompressedOffset;
-        public readonly int UncompressedSize;
+        public readonly long UncompressedOffset;
+        public readonly long UncompressedSize;
         public readonly int CompressedOffset;
         public readonly int CompressedSize;
 
@@ -14,14 +14,14 @@ namespace CUE4Parse.UE4.Assets.Objects
         {
             if (Ar.Game == EGame.GAME_RocketLeague)
             {
-                UncompressedOffset = Ar.Read<int>();
-                UncompressedSize = Ar.Read<int>();
-                goto streamStandardSize;
+                UncompressedOffset = Ar.Read<long>();
+                UncompressedSize = Ar.Read<long>();
+                goto SkipToCompressed;
             }
 
             UncompressedOffset = Ar.Read<int>();
             UncompressedSize = Ar.Read<int>();
-            streamStandardSize:
+            SkipToCompressed:
             CompressedOffset = Ar.Read<int>();
             CompressedSize = Ar.Read<int>();
         }
